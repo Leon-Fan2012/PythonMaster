457a2d00557fb8f704aa3b72102cc670c08f655d
Léon <lemelonjaune2.0@gmail.com>
2026-03-28 14:47:07 +0100

    week 1

---
+++ Chapter1/Course/01_object.py
@@ -0,0 +1,11 @@
+# Créer une classe
+class Student:
+    name = None
+
+# Création d'objet basés sur des classe
+stu_1 = Student()
+stu_2 = Student()
+
+# Attributin d'une valeur d'attribut name à l'objet stu_1
+stu_1.name = "Léon"
+stu_2.name = "Lucie"
